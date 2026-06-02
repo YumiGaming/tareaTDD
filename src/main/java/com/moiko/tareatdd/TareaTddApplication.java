f5bcd0297a4164e897c37fef2794f8bb0441065c
YumiGaming <benjamindamian11032006@gmail.com>
2026-06-02 16:06:50 -0400

    Initial commit: Spring Boot project boilerplate

---
+++ src/main/java/com/moiko/tareatdd/TareaTddApplication.java
@@ -0,0 +1,13 @@
+package com.moiko.tareatdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moiko/tareatdd/TareaTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moiko.tareatdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
